dec8a973b7f1d8749eaf7efab7069e80d200ff4e
abbas <abbas.modhar@swibit.com>
2025-09-23 17:09:09 +0300

    generate a guss number game

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-for (let i=0;i<10;i++){
-   if(i==7){
-      break;
-   }else{
-      console.log(i);
-   }
+const minMam = 1;
+const maxMam = 100;
+const answer = Math.round(Math.random() * (maxMam - minMam + 1)) + minMam;
+let attemp = 0;
+let guess;
+let running = true;
+while (running) {
+  guess = window.prompt(`خمن رقم ما بين ${minMam} و ${maxMam}`);
+  guess = Number(guess);
+  console.log(typeof guess,guess);
+  if (isNaN(guess)) {
+    window.alert("الرقم المدخل غير صحيح");
+  } else if (guess < minMam || guess > maxMam) {
+    window.alert("الرقم المدخل غير صحيح");
+  } else {
+    attemp++;
+    if (guess < answer ) {
+      window.alert("الرقم المدخل أقل من الرقم المفترض");
+    } else if (guess > answer) {
+      window.alert("الرقم المدخل أكبر من الرقم المفترض");
+    } else {
+      window.alert(`تم العثور على الرقم ${answer} ب ${attemp} محاولة`);
+      running=false;
+    }
+
+    
+  }
 }
