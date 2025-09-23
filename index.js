144288f2b23ce98730e226963660ccf3af3ec597
abbas <abbas.modhar@swibit.com>
2025-09-23 18:18:49 +0300

    temp app

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-const minMam = 1;
-const maxMam = 100;
-const answer = Math.round(Math.random() * (maxMam - minMam + 1)) + minMam;
-let attemp = 0;
-let guess;
-let running = true;
-while (running) {
-  guess = window.prompt(`خمن رقم ما بين ${minMam} و ${maxMam}`);
-  guess = Number(guess);
-  console.log(typeof guess,guess);
-  if (isNaN(guess)) {
-    window.alert("الرقم المدخل غير صحيح");
-  } else if (guess < minMam || guess > maxMam) {
-    window.alert("الرقم المدخل غير صحيح");
-  } else {
-    attemp++;
-    if (guess < answer ) {
-      window.alert("الرقم المدخل أقل من الرقم المفترض");
-    } else if (guess > answer) {
-      window.alert("الرقم المدخل أكبر من الرقم المفترض");
-    } else {
-      window.alert(`تم العثور على الرقم ${answer} ب ${attemp} محاولة`);
-      running=false;
-    }
-
-    
-  }
-}
+ const tempBox=document.getElementById("tempBox");
+ const toF=document.getElementById("toF");
+ const toC=document.getElementById("toC");
+ const result=document.getElementById("result");
+ 
+ function convert(){
+   if(toF.checked){
+      temp=Number(tempBox.value);
+      temp=temp*9/5+32;
+      result.textContent=temp.toFixed(1) +' F';
+   }else if(toC.checked){
+      temp=Number(tempBox.value);
+      temp=(temp-32)*5/9;
+      result.textContent=temp.toFixed(1) +' C';
+   }
+ }
