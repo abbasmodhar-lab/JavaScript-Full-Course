947bf99ade0049df054850a2bed69f1b50b2cab2
abbas <abbas.modhar@swibit.com>
2025-09-24 21:40:39 +0300

    simple hello world alert function and set a timeout for execution

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
- 
-//  constructor function
- function Car(make,model,year,color){
-   this.make=make;
-   this.model=model;
-   this.year=year;
-   this.color=color;
-  this.drive=function(){
-    console.log(`Driving ${this.make} ${this.model} ${this.year} ${this.color}`);
-  }
- }
- //  creating new objects
- const car1=new Car('Toyota','Corolla',2020,'Red');
- const car2=new Car('Chevrolet','Camaro',2021,'Blue');
- console.log(car1);
- //  printing the objects
- console.log(car2);
 
- car1.drive();
- car2.drive();
+function sayHello(){
+   window.alert('Hello, world!');
+}
+setTimeout(sayHello,2000);
