b67685540c19fb6b7438425bb8ef71bcff0cc188
abbas <abbas.modhar@swibit.com>
2025-09-24 19:51:09 +0300

    Refactor code to implement a Car constructor function and create car objects with drive methods

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const person1 = {
-  firstName: "abbas",
-  lastName: "modhar",
-  age: 25,
-  gender: "male",
-  sayHello: function() {
-    console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
-  },
-   
-};
-person1.sayHello();
+ 
+//  constructor function
+ function Car(make,model,year,color){
+   this.make=make;
+   this.model=model;
+   this.year=year;
+   this.color=color;
+  this.drive=function(){
+    console.log(`Driving ${this.make} ${this.model} ${this.year} ${this.color}`);
+  }
+ }
+ //  creating new objects
+ const car1=new Car('Toyota','Corolla',2020,'Red');
+ const car2=new Car('Chevrolet','Camaro',2021,'Blue');
+ console.log(car1);
+ //  printing the objects
+ console.log(car2);
+
+ car1.drive();
+ car2.drive();
