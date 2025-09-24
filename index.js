d31c8362b21ce25f8b2b6683a869713452c83c5b
abbas <abbas.modhar@swibit.com>
2025-09-24 16:45:22 +0300

    generate a generateor password app

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
- const tempBox=document.getElementById("tempBox");
- const toF=document.getElementById("toF");
- const toC=document.getElementById("toC");
- const result=document.getElementById("result");
- 
- function convert(){
-   if(toF.checked){
-      temp=Number(tempBox.value);
-      temp=temp*9/5+32;
-      result.textContent=temp.toFixed(1) +' F';
-   }else if(toC.checked){
-      temp=Number(tempBox.value);
-      temp=(temp-32)*5/9;
-      result.textContent=temp.toFixed(1) +' C';
-   }
- }
+function generatePassword(
+  length,
+  includeLowerCase,
+  includeUpperCase,
+  includeNumbers,
+  includeSymbols
+) {
+   const lowerCaseLetters="abcdefghijklmnopqrstuvwxyz";
+   const upperCaseLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   const numbers="0123456789";
+   const symbols="!@#$%^&*()_+-=[]{}|;:,.<>?";
+   let allowedCharacters="";
+   let password="";
+   if(includeLowerCase) allowedCharacters+=lowerCaseLetters;
+   if(includeUpperCase) allowedCharacters+=upperCaseLetters;
+   if(includeNumbers) allowedCharacters+=numbers;
+   if(includeSymbols) allowedCharacters+=symbols;
+   if(length<=0) return "Password length must be greater than 0";
+   if(allowedCharacters.length===0) return "At least one character type must be selected";
+    for(let i=0;i<length;i++){
+      const randomIndex=Math.floor(Math.random()*allowedCharacters.length);
+      password+=allowedCharacters[randomIndex];
+    }
+   return password;
+  
+}
+const passwordLength = 12;
+const includeLowerCase = false;
+const includeUpperCase = true; 
+const includeNumbers = true;
+const includeSymbols = true;
+const password = generatePassword(
+  passwordLength,
+  includeLowerCase,
+  includeUpperCase,
+  includeNumbers,
+  includeSymbols
+);
+console.log(`Generated Password: ${password}`);
